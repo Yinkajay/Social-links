dd328034a00e24531adc759d169fb30a594822c5
Adeyinka Junaid <yinkajunn@gmail.com>
2024-04-27 18:46:54 +0100

    Create layout and add responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
-import { useState } from 'react'
 import './App.css'
+import avatar from '../assets/images/avatar-jessica.jpeg'
 
 function App() {
 
   return (
-    <>
-  Jessica Randall
-  London, United Kingdom
-  "Front-end developer and avid reader."
+    <div className='bg-[#1f1f1f] text-white w-80 sm:w-96 p-6 rounded-md text-center'>
+      <img src={avatar} alt="avatar" className='rounded-full w-24 h-24 mx-auto mb-4' />
+      <h1 className='text-3xl'>
+        Jessica Randall
+      </h1>
+      <h2 className='text-[#c5f82a]'>London, United Kingdom</h2>
+      <p className='my-6'>
+        &quot;Front-end developer and avid reader.&quot;
+      </p>
 
-  GitHub
-  Frontend Mentor
-  LinkedIn
-  Twitter
-  Instagram
-  
-  <div class="attribution">
-    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-    Coded by <a href="#">Your Name Here</a>.
-  </div>
-    </>
+      <ul>
+        <li className='bg-[#333333] rounded-md p-2 my-2 hover:text-[#1f1f1f] hover:bg-[#c5f82a] hover:cursor-pointer'>GitHub</li>
+        <li className='bg-[#333333] rounded-md p-2 my-2 hover:text-[#1f1f1f] hover:bg-[#c5f82a] hover:cursor-pointer'>Frontend Mentor</li>
+        <li className='bg-[#333333] rounded-md p-2 my-2 hover:text-[#1f1f1f] hover:bg-[#c5f82a] hover:cursor-pointer'>LinkedIn</li>
+        <li className='bg-[#333333] rounded-md p-2 my-2 hover:text-[#1f1f1f] hover:bg-[#c5f82a] hover:cursor-pointer'>Twitter</li>
+        <li className='bg-[#333333] rounded-md p-2 my-2 hover:text-[#1f1f1f] hover:bg-[#c5f82a] hover:cursor-pointer'>Instagram</li>
+      </ul>
+    </div>
   )
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+const Footer = () => {
+    return (
+        <div>
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+            Coded by <a href="#">Your Name Here</a>.
+        </div>
+    )
+}
+
+export default Footer
