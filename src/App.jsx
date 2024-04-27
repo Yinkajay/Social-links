dd2939e897dcac8039bb23cd4ff333a08f68dd74
Adeyinka Junaid <yinkajunn@gmail.com>
2024-04-28 00:53:07 +0100

    Add responsiveness and attribution

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,49 @@
 import './App.css'
 import avatar from '../assets/images/avatar-jessica.jpeg'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
-    <div className='bg-[#1f1f1f] text-white w-80 sm:w-96 p-6 rounded-md text-center'>
+    <>
+    <div className='bg-[#1f1f1f] text-white sm:p-4 sm:w-96 p-6 rounded-md text-center font-Inter'>
       <img src={avatar} alt="avatar" className='rounded-full w-24 h-24 mx-auto mb-4' />
-      <h1 className='text-3xl'>
+      <h1 className='text-2xl font-semibold my-3'>
         Jessica Randall
       </h1>
-      <h2 className='text-[#c5f82a]'>London, United Kingdom</h2>
-      <p className='my-6'>
+      <h2 className='text-[#c5f82a] text-xs font-semibold'>London, United Kingdom</h2>
+      <p className='my-6 text-sm'>
         &quot;Front-end developer and avid reader.&quot;
       </p>
 
       <ul>
-        <li className='bg-[#333333] rounded-md p-2 my-2 hover:text-[#1f1f1f] hover:bg-[#c5f82a] hover:cursor-pointer'>GitHub</li>
-        <li className='bg-[#333333] rounded-md p-2 my-2 hover:text-[#1f1f1f] hover:bg-[#c5f82a] hover:cursor-pointer'>Frontend Mentor</li>
-        <li className='bg-[#333333] rounded-md p-2 my-2 hover:text-[#1f1f1f] hover:bg-[#c5f82a] hover:cursor-pointer'>LinkedIn</li>
-        <li className='bg-[#333333] rounded-md p-2 my-2 hover:text-[#1f1f1f] hover:bg-[#c5f82a] hover:cursor-pointer'>Twitter</li>
-        <li className='bg-[#333333] rounded-md p-2 my-2 hover:text-[#1f1f1f] hover:bg-[#c5f82a] hover:cursor-pointer'>Instagram</li>
+        <a href='https://github.com/Yinkajay'>
+          <li className='bg-[#333333] rounded-md p-2 my-2 hover:text-[#1f1f1f] hover:bg-[#c5f82a] hover:cursor-pointer font-semibold'>GitHub</li>
+        </a>
+
+        <a href='https://www.frontendmentor.io/profile/Yinkajay'>
+          <li className='bg-[#333333] rounded-md p-2 my-2 hover:text-[#1f1f1f] hover:bg-[#c5f82a] hover:cursor-pointer font-semibold'>Frontend Mentor</li>
+        </a>
+
+        <a href='https://www.linkedin.com/in/yinka-junaid-a9a654171/'>
+          <li className='bg-[#333333] rounded-md p-2 my-2 hover:text-[#1f1f1f] hover:bg-[#c5f82a] hover:cursor-pointer font-semibold'>LinkedIn</li>
+        </a>
+
+        <a href='https://www.linkedin.com/in/yinka-junaid-a9a654171/'>
+          <li className='bg-[#333333] rounded-md p-2 my-2 hover:text-[#1f1f1f] hover:bg-[#c5f82a] hover:cursor-pointer font-semibold'>Twitter</li>
+        </a>
+
+        <a href='https://www.instagram.com/yinka_jun/'>
+          <li className='bg-[#333333] rounded-md p-2 my-2 hover:text-[#1f1f1f] hover:bg-[#c5f82a] hover:cursor-pointer font-semibold'>Instagram</li>
+        </a>
+
       </ul>
+
+      
     </div>
+
+    <Footer />
+    </>
   )
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 
 const Footer = () => {
     return (
-        <div>
+        <div className="text-white text-sm left-1/2 -translate-x-1/2 absolute bottom-3">
             Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
-            Coded by <a href="#">Your Name Here</a>.
+            Coded by <a className="text-[#3e52a3]" href="https://www.frontendmentor.io/profile/Yinkajay">Yinx</a>.
         </div>
     )
 }
